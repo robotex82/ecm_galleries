8679b58659fb8d3d17c20ed3184188f418ed6df0
Roberto Vasquez Angel <roberto@vasquez-angel.de>
2018-12-26 01:24:42 +0100

    Add thumbnail_variant_options configuration option. Cleanup.

--- lib/generators/ecm/galleries/install/templates/initializer.rb
+++ lib/generators/ecm/galleries/install/templates/initializer.rb
@@ -10,12 +10,18 @@ Ecm::Galleries.configure do |config|
   #
   #  Default: config.galleries_helper_render_default_options = { variant_options: {}, show_details: false }
   #
-  config.base_controller = '<%= base_controller_class_name %>'
+  config.galleries_helper_render_default_options = { variant_options: {}, show_details: false }
 
   # These options are the defaults that will be applied to when rendering
   # pictures through the pictures helper.
   #
-  #  Default: config.pictures_helper_render_default_options = { variant_options: {}, show_details: false }
+  #  Default: config.pictures_helper_render_default_options = { image_tag_only: true }
   #
-  config.pictures_helper_render_default_options = { variant_options: {}, show_details: false }
+  config.pictures_helper_render_default_options = { image_tag_only: true }
+
+  # This options will be applied when rendering thumbails in galleries.
+  #
+  # Default: config.thumbnail_variant_options = { combine_options: { resize: "384x216^", extent: "384x216", gravity: "center" } }
+  #
+  config.thumbnail_variant_options = { combine_options: { resize: "384x216^", extent: "384x216", gravity: "center" } }
 end
